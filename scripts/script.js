5d0776125c62439dbcd90bb33bd2da11d181a871
Jin Park <103798287+jinftw64@users.noreply.github.com>
2023-10-31 11:45:20 -0400

    icons populate from the board array onto the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const gameBoard = [
-  null, null, null,
-  null, null, null,
+  'x', null, null,
+  null, 'x', null,
   null, null, null,
 ]
 
@@ -14,13 +14,13 @@ const displayController = (function() {
 
     switch (element) {
       case 'x':
-        divClass = '.x';
+        divClass = 'x';
         break;
       case 'o':
-        divClass = '.o';
+        divClass = 'o';
         break
       default:
-        divClass = '.blank';
+        divClass = 'blank';
     }
 
     currentDiv.classList.add(divClass);
@@ -32,7 +32,28 @@ const displayController = (function() {
     gameBoard.forEach((element) => boardDiv.appendChild(createDiv(element)));
   }
 
-  return { populateBoard }
+  const assignIcons = function() {
+    const allXMarkers = document.querySelectorAll('.x');
+    const allOMarkers = document.querySelectorAll('.o');
+
+    const xImagePath = '../images/close-thick.svg';
+    const oImagePath = '../images/circle-outline.svg';
+
+    const addImgElement = function(array, imagePath) {
+
+      array.forEach(function(element) {
+        const img = document.createElement('img');
+        img.src = imagePath;
+        element.appendChild(img);
+      })
+    }
+
+    addImgElement(allXMarkers, xImagePath);
+    addImgElement(allOMarkers, oImagePath);
+  }
+
+  return { populateBoard, assignIcons }
 })();
 
 displayController.populateBoard();
+displayController.assignIcons();
